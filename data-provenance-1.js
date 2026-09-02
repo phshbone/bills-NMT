2b62008574d68869f51c9463a6276b38e569650d
phshbone <phshbone@gmail.com>
2026-09-02 18:36:38 -0400

    Deepen upper-quarter source provenance

--- data-provenance-1.js
+++ data-provenance-1.js
@@ -1,6 +1,9 @@
 Object.assign(window.NMT_DATA.SOURCES,{
   "levator_statpearls":{"id":"levator_statpearls","title":"Anatomy, Head and Neck, Levator Scapulae Muscles","publisher":"StatPearls / NCBI Bookshelf","url":"https://www.ncbi.nlm.nih.gov/books/NBK553120/","type":"current anatomy reference"},
   "scm_statpearls":{"id":"scm_statpearls","title":"Anatomy, Head and Neck: Sternocleidomastoid Muscle","publisher":"StatPearls / NCBI Bookshelf","url":"https://www.ncbi.nlm.nih.gov/books/NBK532881/","type":"current anatomy reference"},
+  "scalenus_statpearls":{"id":"scalenus_statpearls","title":"Anatomy, Head and Neck, Scalenus Muscle","publisher":"StatPearls / NCBI Bookshelf","url":"https://www.ncbi.nlm.nih.gov/books/NBK519058/","type":"current anatomy reference"},
+  "long_thoracic_statpearls":{"id":"long_thoracic_statpearls","title":"Anatomy, Thorax, Long Thoracic Nerve","publisher":"StatPearls / NCBI Bookshelf","url":"https://www.ncbi.nlm.nih.gov/sites/books/NBK535396/","type":"current anatomy reference"},
+  "scapula_statpearls":{"id":"scapula_statpearls","title":"Anatomy, Thorax, Scapula","publisher":"StatPearls / NCBI Bookshelf","url":"https://www.ncbi.nlm.nih.gov/books/NBK538319/","type":"current anatomy reference"},
   "trapezius_statpearls":{"id":"trapezius_statpearls","title":"Anatomy, Back, Trapezius","publisher":"StatPearls / NCBI Bookshelf","url":"https://www.ncbi.nlm.nih.gov/books/NBK518994/","type":"current anatomy reference"},
   "latissimus_statpearls":{"id":"latissimus_statpearls","title":"Anatomy, Back, Latissimus Dorsi","publisher":"StatPearls / NCBI Bookshelf","url":"https://www.ncbi.nlm.nih.gov/books/NBK448120/","type":"current anatomy reference"},
   "shoulder_muscles_statpearls":{"id":"shoulder_muscles_statpearls","title":"Anatomy, Shoulder and Upper Limb, Shoulder Muscles","publisher":"StatPearls / NCBI Bookshelf","url":"https://www.ncbi.nlm.nih.gov/books/NBK534836/","type":"current anatomy reference"},
@@ -9,12 +12,13 @@ Object.assign(window.NMT_DATA.SOURCES,{
 });
 
 const provenance={
-  "scalenes":["neck_movements_statpearls"],
-  "sternocleidomastoid":["scm_statpearls"],
-  "levator-scapulae":["levator_statpearls"],
-  "upper-trapezius":["trapezius_statpearls"],
-  "middle-trapezius":["trapezius_statpearls"],
-  "lower-trapezius":["trapezius_statpearls"],
+  "serratus-anterior":["serratus_statpearls","long_thoracic_statpearls","scapula_statpearls"],
+  "scalenes":["scalenus_statpearls","neck_movements_statpearls"],
+  "sternocleidomastoid":["scm_statpearls","neck_movements_statpearls"],
+  "levator-scapulae":["levator_statpearls","neck_movements_statpearls","scapula_statpearls"],
+  "upper-trapezius":["trapezius_statpearls","scapula_statpearls"],
+  "middle-trapezius":["trapezius_statpearls","scapula_statpearls"],
+  "lower-trapezius":["trapezius_statpearls","scapula_statpearls"],
   "pectoralis-minor":["pectoral_statpearls","shoulder_muscles_statpearls"],
   "latissimus-dorsi":["latissimus_statpearls"],
   "rotator-cuff":["shoulder_muscles_statpearls"],
